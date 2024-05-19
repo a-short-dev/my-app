a1ec60ee534f292931697c70f038e0df433c5209
Leke Abiodun <lakessyde1@gmail.com>
2024-05-19 21:30:28 +0100

    initial commit

---
+++ constants/Styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import { Colors } from "./Colors";
+
+export const defaultStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: "700",
+  },
+  textLink: {
+    color:Colors.
+  },
+});
